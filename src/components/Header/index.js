8bf90204f7fec48b75a81076ed4f1a249d0dd182
edu <edu.slhenrique@gmail.com>
2022-11-01 21:44:48 -0300

    # Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+const Header = () => {
+    return <header>
+        <img src="logo.svg" alt="Logo"/>
+        <nav>
+            <ul>
+                <li>
+                    Dados empresa
+                </li>
+                <li>
+                    Cadastro vagas
+                </li>
+                <li>
+                    Resultados
+                </li>
+            </ul>
+        </nav>
+    </header>
+}
+
+export default Header;
