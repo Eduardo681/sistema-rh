fb8c71b93c738a849afd06b781a134cd5118bb50
JoaoVitorFranca <joaovitorfranca000@gmail.com>
2022-11-07 20:15:36 -0300

    inclusao tela de cadastro vaga

--- frontend/src/pages/CadastroVaga/index.js
+++ frontend/src/pages/CadastroVaga/index.js
@@ -1,6 +1,28 @@
 import Header from "../../components/Header";
+import { useState} from "react";
+import axios from "axios";
+
 
 const CadastroVaga = () => {
+    const [vaga, setVaga] = useState({
+        nome_vaga: '',
+        salario: 0,
+        plano_de_carreira: '',
+        descricao: '',
+        perc_acerto_logico: 0,
+        perc_acerto_interpertacao: 0,
+        perc_acerto_tecnico: 0,
+        requisitos: '',
+        diferenciais: '',
+        empresa: 1
+    });
+
+    const salvarVaga = () => {
+        console.log(vaga)
+        axios.post("/vaga", vaga).then((r) => {
+            alert(r.data)
+        }).catch((e) => console.log(e))
+    }
     return <>
         <Header />
         <div className="container">
@@ -8,19 +30,58 @@ const CadastroVaga = () => {
                 Dados vaga
             </div>
             <form>
-                <div className="form-control">
-                    <label htmlFor="vaga">Nome da vaga</label>
-                    <input type="text" name="vaga" />
+                <div className="side">
+                    <div className="form-control">
+                        <label htmlFor="vaga">Nome da vaga</label>
+                        <input type="text" name="vaga"
+                            onChange={t => setVaga({...vaga, nome_vaga: t.target.value})} />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="requisitos">Requisitos</label>
+                        <select type="text" name="requisitos" onChange={t => setVaga({...vaga, requisitos: t.target.value})}>
+                            <option value="teste">teste</option>
+                        </select>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="vaga">Plano de carreira</label>
+                        <input type="text" name="vaga" 
+                            onChange={t => setVaga({...vaga, plano_de_carreira: t.target.value})}/>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="vaga">Percentual de acerto do teste de interpretacao (%)</label>
+                        <input type="number" name="vaga"
+                            onChange={t => setVaga({...vaga, perc_acerto_interpertacao: t.target.value})} />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="vaga">Percentual de acerto do teste de logico (%)</label>
+                        <input type="number" name="vaga" 
+                            onChange={t => setVaga({...vaga, perc_acerto_logico: t.target.value})}/>
+                    </div>
                 </div>
-                <div className="form-control">
-                    <label htmlFor="requisitos">Requisitos</label>
-                    <select type="text" name="requisitos">
-                        <option value="teste">teste</option>
-                    </select>
-                </div>
-                <div className="form-control">
-                    <label htmlFor="carreita">Plano de carreira</label>
-                    <textarea type="text" name="carreira" />
+                <div className="side">
+                    <div className="form-control">
+                        <label htmlFor="vaga">Salario</label>
+                        <input type="number" name="vaga" 
+                            onChange={t => setVaga({...vaga, salario: t.target.value})}/>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="requisitos">Diferencias</label>
+                        <select type="text" name="requisitos" onChange={t => setVaga({...vaga, diferencias: t.target.value})}>
+                            <option value="teste">alt</option>
+                        </select>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="vaga">Descricao</label>
+                        <input type="text" name="vaga"
+                            onChange={t => setVaga({...vaga, descricao: t.target.value})}/>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="vaga">Percentual de acerto do teste tecnico (%)</label>
+                        <input type="number" name="vaga" 
+                            onChange={t => setVaga({...vaga, perc_acerto_tecnico: t.target.value})}/>
+                    </div>
+                    
+                <button type={"button"} onClick={salvarVaga}> Salvar</button>   
                 </div>
             </form>
         </div>
